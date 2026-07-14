21ffa8e4f6f822e6adb8a9749afb4440e96a3772
Salu2001 <143968974+Salu2001@users.noreply.github.com>
2026-07-14 19:11:28 +0300

    Delete __init__.py

--- handlers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .start import start
-from .help import help_command
-#from .download import download
-# Import other handlers as needed
-
-__all__ = ["start", "help_command"]  # Add all functions you want to export
